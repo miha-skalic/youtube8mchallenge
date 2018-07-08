ca90f85816b75c51c84a6c53349239790ef35e8d
David Austin <daustingm1@gmail.com>
2018-07-08 04:19:08 +0000

    add FVnets

--- devel_models.py
+++ devel_models.py
@@ -378,3 +378,217 @@ class NetVLADModelLF(models.BaseModel):
             vocab_size=vocab_size,
             is_training=is_training,
             **unused_params)
+
+class NetFVModelLF(models.BaseModel):
+  """Creates a NetFV based model.
+     It emulates a Gaussian Mixture Fisher Vector pooling operations
+
+  Args:
+    model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
+                 input features.
+    vocab_size: The number of classes in the dataset.
+    num_frames: A vector of length 'batch' which indicates the number of
+         frames for each video (before padding).
+
+  Returns:
+    A dictionary with a tensor containing the probability predictions of the
+    model in the 'predictions' key. The dimensions of the tensor are
+    'batch_size' x 'num_classes'.
+  """
+
+
+  def create_model(self,
+                   model_input,
+                   vocab_size,
+                   num_frames,
+                   iterations=None,
+                   add_batch_norm=None,
+                   sample_random_frames=None,
+                   cluster_size=None,
+                   hidden_size=None,
+                   is_training=True,
+                   **unused_params):
+    iterations = iterations or FLAGS.iterations
+    add_batch_norm = add_batch_norm or FLAGS.netvlad_add_batch_norm
+    random_frames = sample_random_frames or FLAGS.sample_random_frames
+    cluster_size = cluster_size or FLAGS.fv_cluster_size
+    hidden1_size = hidden_size or FLAGS.fv_hidden_size
+    relu = FLAGS.fv_relu
+    gating = FLAGS.gating
+
+    num_frames = tf.cast(tf.expand_dims(num_frames, 1), tf.float32)
+    if random_frames:
+      model_input = utils.SampleRandomFrames(model_input, num_frames,
+                                             iterations)
+    else:
+      model_input = utils.SampleRandomSequence(model_input, num_frames,
+                                               iterations)
+    max_frames = model_input.get_shape().as_list()[1]
+    feature_size = model_input.get_shape().as_list()[2]
+    reshaped_input = tf.reshape(model_input, [-1, feature_size])
+    tf.summary.histogram("input_hist", reshaped_input)
+
+    video_NetFV = NetFV(1024,max_frames,cluster_size, add_batch_norm, is_training)
+    audio_NetFV = NetFV(128,max_frames,cluster_size/2, add_batch_norm, is_training)
+
+
+    if add_batch_norm:
+      reshaped_input = slim.batch_norm(
+          reshaped_input,
+          center=True,
+          scale=True,
+          is_training=is_training,
+          scope="input_bn")
+
+    with tf.variable_scope("video_FV"):
+        fv_video = video_NetFV.forward(reshaped_input[:,0:1024]) 
+
+    with tf.variable_scope("audio_FV"):
+        fv_audio = audio_NetFV.forward(reshaped_input[:,1024:])
+
+    fv = tf.concat([fv_video, fv_audio],1)
+
+    fv_dim = fv.get_shape().as_list()[1] 
+    hidden1_weights = tf.get_variable("hidden1_weights",
+      [fv_dim, hidden1_size],
+      initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(cluster_size)))
+    
+    activation = tf.matmul(fv, hidden1_weights)
+
+    if add_batch_norm and relu:
+      activation = slim.batch_norm(
+          activation,
+          center=True,
+          scale=True,
+          is_training=is_training,
+          scope="hidden1_bn")
+    else:
+      hidden1_biases = tf.get_variable("hidden1_biases",
+        [hidden1_size],
+        initializer = tf.random_normal_initializer(stddev=0.01))
+      tf.summary.histogram("hidden1_biases", hidden1_biases)
+      activation += hidden1_biases
+   
+    if relu:
+      activation = tf.nn.relu6(activation)
+
+    if gating:
+        gating_weights = tf.get_variable("gating_weights_2",
+          [hidden1_size, hidden1_size],
+          initializer = tf.random_normal_initializer(stddev=1 / math.sqrt(hidden1_size)))
+        
+        gates = tf.matmul(activation, gating_weights)
+        
+        if add_batch_norm:
+          gates = slim.batch_norm(
+              gates,
+              center=True,
+              scale=True,
+              is_training=is_training,
+              scope="gating_bn")
+        else:
+          gating_biases = tf.get_variable("gating_biases",
+            [cluster_size],
+            initializer = tf.random_normal(stddev=1 / math.sqrt(feature_size)))
+          gates += gating_biases
+
+        gates = tf.sigmoid(gates)
+
+        activation = tf.multiply(activation,gates)
+
+
+    aggregated_model = getattr(video_level_models,
+                               FLAGS.video_level_classifier_model)
+
+    return aggregated_model().create_model(
+        model_input=activation,
+        vocab_size=vocab_size,
+        is_training=is_training,
+        **unused_params)
+
+class NetFV():
+    def __init__(self, feature_size,max_frames,cluster_size, add_batch_norm, is_training):
+        self.feature_size = feature_size
+        self.max_frames = max_frames
+        self.is_training = is_training
+        self.add_batch_norm = add_batch_norm
+        self.cluster_size = cluster_size
+
+    def forward(self,reshaped_input):
+        cluster_weights = tf.get_variable("cluster_weights",
+          [self.feature_size, self.cluster_size],
+          initializer = tf.random_normal_initializer(stddev=1 / math.sqrt(self.feature_size)))
+     
+        covar_weights = tf.get_variable("covar_weights",
+          [self.feature_size, self.cluster_size],
+          initializer = tf.random_normal_initializer(mean=1.0, stddev=1 /math.sqrt(self.feature_size)))
+      
+        covar_weights = tf.square(covar_weights)
+        eps = tf.constant([1e-6])
+        covar_weights = tf.add(covar_weights,eps)
+
+        tf.summary.histogram("cluster_weights", cluster_weights)
+        activation = tf.matmul(reshaped_input, cluster_weights)
+        if self.add_batch_norm:
+          activation = slim.batch_norm(
+              activation,
+              center=True,
+              scale=True,
+              is_training=self.is_training,
+              scope="cluster_bn")
+        else:
+          cluster_biases = tf.get_variable("cluster_biases",
+            [self.cluster_size],
+            initializer = tf.random_normal(stddev=1 / math.sqrt(self.feature_size)))
+          tf.summary.histogram("cluster_biases", cluster_biases)
+          activation += cluster_biases
+        
+        activation = tf.nn.softmax(activation)
+        tf.summary.histogram("cluster_output", activation)
+
+        activation = tf.reshape(activation, [-1, self.max_frames, self.cluster_size])
+
+        a_sum = tf.reduce_sum(activation,-2,keep_dims=True)
+
+        if not FLAGS.fv_couple_weights:
+            cluster_weights2 = tf.get_variable("cluster_weights2",
+              [1,self.feature_size, self.cluster_size],
+              initializer = tf.random_normal_initializer(stddev=1 / math.sqrt(self.feature_size)))
+        else:
+            cluster_weights2 = tf.scalar_mul(FLAGS.fv_coupling_factor,cluster_weights)
+
+        a = tf.multiply(a_sum,cluster_weights2)
+        
+        activation = tf.transpose(activation,perm=[0,2,1])
+        
+        reshaped_input = tf.reshape(reshaped_input,[-1,self.max_frames,self.feature_size])
+        fv1 = tf.matmul(activation,reshaped_input)
+        
+        fv1 = tf.transpose(fv1,perm=[0,2,1])
+
+        # computing second order FV
+        a2 = tf.multiply(a_sum,tf.square(cluster_weights2)) 
+
+        b2 = tf.multiply(fv1,cluster_weights2) 
+        fv2 = tf.matmul(activation,tf.square(reshaped_input)) 
+     
+        fv2 = tf.transpose(fv2,perm=[0,2,1])
+        fv2 = tf.add_n([a2,fv2,tf.scalar_mul(-2,b2)])
+
+        fv2 = tf.divide(fv2,tf.square(covar_weights))
+        fv2 = tf.subtract(fv2,a_sum)
+
+        fv2 = tf.reshape(fv2,[-1,self.cluster_size*self.feature_size])
+      
+        fv2 = tf.nn.l2_normalize(fv2,1)
+        fv2 = tf.reshape(fv2,[-1,self.cluster_size*self.feature_size])
+        fv2 = tf.nn.l2_normalize(fv2,1)
+
+        fv1 = tf.subtract(fv1,a)
+        fv1 = tf.divide(fv1,covar_weights) 
+
+        fv1 = tf.nn.l2_normalize(fv1,1)
+        fv1 = tf.reshape(fv1,[-1,self.cluster_size*self.feature_size])
+        fv1 = tf.nn.l2_normalize(fv1,1)
+
+        return tf.concat([fv1,fv2],1)
