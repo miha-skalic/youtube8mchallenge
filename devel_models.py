522b856338663be2c4639ed2e2b990d1a4fa0bf7
Miha Skalic <miha.skalic@gmail.com>
2018-07-08 14:31:17 +0200

    soft-DBoF added.

--- devel_models.py
+++ devel_models.py
@@ -64,6 +64,7 @@ flags.DEFINE_bool("fv_couple_weights", True,
 flags.DEFINE_float("fv_coupling_factor", 0.01,
                    "Coupling factor")
 
+
 class attention_frames_v0(models.BaseModel):
     def create_model(self,
                      model_input,
@@ -592,3 +593,362 @@ class NetFV():
         fv1 = tf.nn.l2_normalize(fv1,1)
 
         return tf.concat([fv1,fv2],1)
+
+
+class GatedDBoF():
+    def __init__(self, feature_size, max_frames, cluster_size, max_pool, add_batch_norm, is_training):
+        self.feature_size = feature_size
+        self.max_frames = max_frames
+        self.is_training = is_training
+        self.add_batch_norm = add_batch_norm
+        self.cluster_size = cluster_size
+        self.max_pool = max_pool
+
+    def forward(self, reshaped_input):
+
+        feature_size = self.feature_size
+        cluster_size = self.cluster_size
+        add_batch_norm = self.add_batch_norm
+        max_frames = self.max_frames
+        is_training = self.is_training
+        max_pool = self.max_pool
+
+        cluster_weights = tf.get_variable("cluster_weights",
+                                          [feature_size, cluster_size],
+                                          initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(feature_size)))
+
+        tf.summary.histogram("cluster_weights", cluster_weights)
+        activation = tf.matmul(reshaped_input, cluster_weights)
+
+        if add_batch_norm:
+            activation = slim.batch_norm(
+                activation,
+                center=True,
+                scale=True,
+                is_training=is_training,
+                scope="cluster_bn")
+        else:
+            cluster_biases = tf.get_variable("cluster_biases",
+                                             [cluster_size],
+                                             initializer=tf.random_normal(stddev=1 / math.sqrt(feature_size)))
+            tf.summary.histogram("cluster_biases", cluster_biases)
+            activation += cluster_biases
+
+        activation = tf.nn.softmax(activation)
+
+        activation = tf.reshape(activation, [-1, max_frames, cluster_size])
+
+        activation_sum = tf.reduce_sum(activation, 1)
+
+        activation_max = tf.reduce_max(activation, 1)
+        activation_max = tf.nn.l2_normalize(activation_max, 1)
+
+        dim_red = tf.get_variable("dim_red",
+                                  [cluster_size, feature_size],
+                                  initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(feature_size)))
+
+        cluster_weights_2 = tf.get_variable("cluster_weights_2",
+                                            [feature_size, cluster_size],
+                                            initializer=tf.random_normal_initializer(
+                                                stddev=1 / math.sqrt(feature_size)))
+
+        tf.summary.histogram("cluster_weights_2", cluster_weights_2)
+
+        activation = tf.matmul(activation_max, dim_red)
+        activation = tf.matmul(activation, cluster_weights_2)
+
+        if add_batch_norm:
+            activation = slim.batch_norm(
+                activation,
+                center=True,
+                scale=True,
+                is_training=is_training,
+                scope="cluster_bn_2")
+        else:
+            cluster_biases = tf.get_variable("cluster_biases_2",
+                                             [cluster_size],
+                                             initializer=tf.random_normal(stddev=1 / math.sqrt(feature_size)))
+            tf.summary.histogram("cluster_biases_2", cluster_biases)
+            activation += cluster_biases
+
+        activation = tf.sigmoid(activation)
+
+        activation = tf.multiply(activation, activation_sum)
+        activation = tf.nn.l2_normalize(activation, 1)
+
+        return activation
+
+class SoftDBoF():
+    def __init__(self, feature_size, max_frames, cluster_size, max_pool, add_batch_norm, is_training):
+        self.feature_size = feature_size
+        self.max_frames = max_frames
+        self.is_training = is_training
+        self.add_batch_norm = add_batch_norm
+        self.cluster_size = cluster_size
+        self.max_pool = max_pool
+
+    def forward(self, reshaped_input):
+
+        feature_size = self.feature_size
+        cluster_size = self.cluster_size
+        add_batch_norm = self.add_batch_norm
+        max_frames = self.max_frames
+        is_training = self.is_training
+        max_pool = self.max_pool
+
+        cluster_weights = tf.get_variable("cluster_weights",
+                                          [feature_size, cluster_size],
+                                          initializer=tf.random_normal_initializer(stddev=1 / math.sqrt(feature_size)))
+
+        tf.summary.histogram("cluster_weights", cluster_weights)
+        activation = tf.matmul(reshaped_input, cluster_weights)
+
+        if add_batch_norm:
+            activation = slim.batch_norm(
+                activation,
+                center=True,
+                scale=True,
+                is_training=is_training,
+                scope="cluster_bn")
+        else:
+            cluster_biases = tf.get_variable("cluster_biases",
+                                             [cluster_size],
+                                             initializer=tf.random_normal(stddev=1 / math.sqrt(feature_size)))
+            tf.summary.histogram("cluster_biases", cluster_biases)
+            activation += cluster_biases
+
+        activation = tf.nn.softmax(activation)
+
+        activation = tf.reshape(activation, [-1, max_frames, cluster_size])
+
+        activation_sum = tf.reduce_sum(activation, 1)
+        activation_sum = tf.nn.l2_normalize(activation_sum, 1)
+
+        if max_pool:
+            activation_max = tf.reduce_max(activation, 1)
+            activation_max = tf.nn.l2_normalize(activation_max, 1)
+            activation = tf.concat([activation_sum, activation_max], 1)
+        else:
+            activation = activation_sum
+
+        return activation
+
+
+class SoftDbofModelLF(models.BaseModel):
+    """Creates a Soft Deep Bag of Frames model.
+    The model projects the features for each frame into a higher dimensional
+    'clustering' space, pools across frames in that space, and then
+    uses a configurable video-level model to classify the now aggregated features.
+    The model will randomly sample either frames or sequences of frames during
+    training to speed up convergence.
+    Args:
+      model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
+                   input features.
+      vocab_size: The number of classes in the dataset.
+      num_frames: A vector of length 'batch' which indicates the number of
+           frames for each video (before padding).
+    Returns:
+      A dictionary with a tensor containing the probability predictions of the
+      model in the 'predictions' key. The dimensions of the tensor are
+      'batch_size' x 'num_classes'.
+    """
+
+    def create_model(self,
+                     model_input,
+                     vocab_size,
+                     num_frames,
+                     iterations=None,
+                     add_batch_norm=None,
+                     sample_random_frames=None,
+                     cluster_size=None,
+                     hidden_size=None,
+                     is_training=True,
+                     **unused_params):
+        iterations = iterations or FLAGS.iterations
+        add_batch_norm = add_batch_norm or FLAGS.dbof_add_batch_norm
+        random_frames = sample_random_frames or FLAGS.sample_random_frames
+        cluster_size = cluster_size or FLAGS.dbof_cluster_size
+        hidden1_size = hidden_size or FLAGS.dbof_hidden_size
+        fc_dimred = FLAGS.fc_dimred
+        relu = FLAGS.dbof_relu
+        max_pool = FLAGS.softdbof_maxpool
+
+        num_frames = tf.cast(tf.expand_dims(num_frames, 1), tf.float32)
+        if random_frames:
+            model_input = utils.SampleRandomFrames(model_input, num_frames,
+                                                   iterations)
+        else:
+            model_input = utils.SampleRandomSequence(model_input, num_frames,
+                                                     iterations)
+        max_frames = model_input.get_shape().as_list()[1]
+        feature_size = model_input.get_shape().as_list()[2]
+        reshaped_input = tf.reshape(model_input, [-1, feature_size])
+        tf.summary.histogram("input_hist", reshaped_input)
+
+        video_Dbof = SoftDBoF(1024, max_frames, cluster_size, max_pool, add_batch_norm, is_training)
+        audio_Dbof = SoftDBoF(128, max_frames, cluster_size / 8, max_pool, add_batch_norm, is_training)
+
+        if add_batch_norm:
+            reshaped_input = slim.batch_norm(
+                reshaped_input,
+                center=True,
+                scale=True,
+                is_training=is_training,
+                scope="input_bn")
+
+        with tf.variable_scope("video_DBOF"):
+            dbof_video = video_Dbof.forward(reshaped_input[:, 0:1024])
+
+        with tf.variable_scope("audio_DBOF"):
+            dbof_audio = audio_Dbof.forward(reshaped_input[:, 1024:])
+
+        dbof = tf.concat([dbof_video, dbof_audio], 1)
+
+        dbof_dim = dbof.get_shape().as_list()[1]
+
+        if fc_dimred:
+            hidden1_weights = tf.get_variable("hidden1_weights",
+                                              [dbof_dim, hidden1_size],
+                                              initializer=tf.random_normal_initializer(
+                                                  stddev=1 / math.sqrt(cluster_size)))
+            tf.summary.histogram("hidden1_weights", hidden1_weights)
+            activation = tf.matmul(dbof, hidden1_weights)
+
+            if add_batch_norm and relu:
+                activation = slim.batch_norm(
+                    activation,
+                    center=True,
+                    scale=True,
+                    is_training=is_training,
+                    scope="hidden1_bn")
+            else:
+                hidden1_biases = tf.get_variable("hidden1_biases",
+                                                 [hidden1_size],
+                                                 initializer=tf.random_normal_initializer(stddev=0.01))
+                tf.summary.histogram("hidden1_biases", hidden1_biases)
+                activation += hidden1_biases
+
+            if relu:
+                activation = tf.nn.relu6(activation)
+            tf.summary.histogram("hidden1_output", activation)
+        else:
+            activation = dbof
+
+        aggregated_model = getattr(video_level_models,
+                                   FLAGS.video_level_classifier_model)
+
+        return aggregated_model().create_model(
+            model_input=activation,
+            vocab_size=vocab_size,
+            is_training=is_training,
+            **unused_params)
+
+
+class GatedDbofModelLF(models.BaseModel):
+    """Creates a Gated Deep Bag of Frames model.
+    The model projects the features for each frame into a higher dimensional
+    'clustering' space, pools across frames in that space, and then
+    uses a configurable video-level model to classify the now aggregated features.
+    The model will randomly sample either frames or sequences of frames during
+    training to speed up convergence.
+    Args:
+      model_input: A 'batch_size' x 'max_frames' x 'num_features' matrix of
+                   input features.
+      vocab_size: The number of classes in the dataset.
+      num_frames: A vector of length 'batch' which indicates the number of
+           frames for each video (before padding).
+    Returns:
+      A dictionary with a tensor containing the probability predictions of the
+      model in the 'predictions' key. The dimensions of the tensor are
+      'batch_size' x 'num_classes'.
+    """
+
+    def create_model(self,
+                     model_input,
+                     vocab_size,
+                     num_frames,
+                     iterations=None,
+                     add_batch_norm=None,
+                     sample_random_frames=None,
+                     cluster_size=None,
+                     hidden_size=None,
+                     is_training=True,
+                     **unused_params):
+        iterations = iterations or FLAGS.iterations
+        add_batch_norm = add_batch_norm or FLAGS.dbof_add_batch_norm
+        random_frames = sample_random_frames or FLAGS.sample_random_frames
+        cluster_size = cluster_size or FLAGS.dbof_cluster_size
+        hidden1_size = hidden_size or FLAGS.dbof_hidden_size
+        fc_dimred = FLAGS.fc_dimred
+        relu = FLAGS.dbof_relu
+        max_pool = FLAGS.softdbof_maxpool
+
+        num_frames = tf.cast(tf.expand_dims(num_frames, 1), tf.float32)
+        if random_frames:
+            model_input = utils.SampleRandomFrames(model_input, num_frames,
+                                                   iterations)
+        else:
+            model_input = utils.SampleRandomSequence(model_input, num_frames,
+                                                     iterations)
+        max_frames = model_input.get_shape().as_list()[1]
+        feature_size = model_input.get_shape().as_list()[2]
+        reshaped_input = tf.reshape(model_input, [-1, feature_size])
+        tf.summary.histogram("input_hist", reshaped_input)
+
+        video_Dbof = GatedDBoF(1024, max_frames, cluster_size, max_pool, add_batch_norm, is_training)
+        audio_Dbof = SoftDBoF(128, max_frames, cluster_size / 8, max_pool, add_batch_norm, is_training)
+
+        if add_batch_norm:
+            reshaped_input = slim.batch_norm(
+                reshaped_input,
+                center=True,
+                scale=True,
+                is_training=is_training,
+                scope="input_bn")
+
+        with tf.variable_scope("video_DBOF"):
+            dbof_video = video_Dbof.forward(reshaped_input[:, 0:1024])
+
+        with tf.variable_scope("audio_DBOF"):
+            dbof_audio = audio_Dbof.forward(reshaped_input[:, 1024:])
+
+        dbof = tf.concat([dbof_video, dbof_audio], 1)
+
+        dbof_dim = dbof.get_shape().as_list()[1]
+
+        if fc_dimred:
+            hidden1_weights = tf.get_variable("hidden1_weights",
+                                              [dbof_dim, hidden1_size],
+                                              initializer=tf.random_normal_initializer(
+                                                  stddev=1 / math.sqrt(cluster_size)))
+            tf.summary.histogram("hidden1_weights", hidden1_weights)
+            activation = tf.matmul(dbof, hidden1_weights)
+
+            if add_batch_norm and relu:
+                activation = slim.batch_norm(
+                    activation,
+                    center=True,
+                    scale=True,
+                    is_training=is_training,
+                    scope="hidden1_bn")
+            else:
+                hidden1_biases = tf.get_variable("hidden1_biases",
+                                                 [hidden1_size],
+                                                 initializer=tf.random_normal_initializer(stddev=0.01))
+                tf.summary.histogram("hidden1_biases", hidden1_biases)
+                activation += hidden1_biases
+
+            if relu:
+                activation = tf.nn.relu6(activation)
+            tf.summary.histogram("hidden1_output", activation)
+        else:
+            activation = dbof
+
+        aggregated_model = getattr(video_level_models,
+                                   FLAGS.video_level_classifier_model)
+
+        return aggregated_model().create_model(
+            model_input=activation,
+            vocab_size=vocab_size,
+            is_training=is_training,
+            **unused_params)
--- runme.md
+++ runme.md
@@ -157,3 +157,40 @@ python train.py \
   --frame_shuffle
   --start_new_model
 ```
+
+```
+python train.py \
+  --train_data_pattern="$RECORDPAT/*.tfrecord" \
+  --model=GatedDbofModelLF \
+  --train_dir="$SAVEPATH/gatedsoftdbof" \
+  --frame_features=True \
+  --feature_names="rgb,audio" \
+  --feature_sizes="1024,128" \
+  --batch_size=80 \
+  --base_learning_rate=0.0002 \
+  --dbof_cluster_size=4096 \
+  --dbof_hidden_size=1024 \
+  --moe_l2=1e-6 \
+  --iterations=300 \
+  --dbof_relu=False \
+  --num_gpu 2 \
+  --num_epochs=7
+```
+
+```
+python train.py \
+  --train_data_pattern="$RECORDPAT/*.tfrecord" \
+  --model=SoftDbofModelLF \
+  --train_dir="$SAVEPATH/softdboflf8000" \
+  --frame_features=True \
+  --feature_names="rgb,audio" \
+  --feature_sizes="1024,128" \
+  --batch_size=80 \
+  --base_learning_rate=0.0002 \
+  --dbof_cluster_size=8000 \
+  --dbof_hidden_size=1024 \
+  --iterations=300 \
+  --dbof_relu=False \
+  --num_gpu 2 \
+  --num_epochs=7
+```
